145d827a9adf160322b41cf6443905b66f0744da
Rodion <rodion.developer1@gmail.com>
2021-09-20 08:17:29 +0000

    Basic blog example (started)

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'index', controller: :welcome
+  root "welcome#index"
+  resources :articles do
+  	resources :comments
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,18 @@
+ActiveRecord::Schema.define(version: 20171206005116) do
+  enable_extension "plpgsql"
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+  add_foreign_key "comments", "articles"
+end
