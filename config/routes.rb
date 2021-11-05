106028ca835c19e16d048a392bd2d266f582abcf
Rodion <rodion.developer1@gmail.com>
2021-11-05 20:51:53 +0000

    Added SessionsController, UsersController, User (model), Authorization (model)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+    get '/auth', to: redirect('/auth/google'), as: 'signin'
+    get '/auth/google/callback', to: 'sessions#create'
+    get '/auth/failure', to: 'sessions#failure'
+    get '/signout', to: 'sessions#destroy', as: 'signout'
     root 'landing#index'
 end
---
+++ db/schema.rb
@@ -0,0 +1,31 @@
+ActiveRecord::Schema.define(version: 20171215035925) do
+  enable_extension "plpgsql"
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  create_table "authorizations", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  add_foreign_key "comments", "articles"
+end
