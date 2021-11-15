6d6eb0039eaa0944a7ad43d180c0482fdc6e43ad
Rodion <rodion.developer1@gmail.com>
2021-11-15 02:44:56 +0000

    Automatically redirects to /dashboard on login from root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
     SITE_NAME = "Bikboo"
     protect_from_forgery with: :exception
+    def url_absolute?(url)
+        /^https?:\/\//i.match? ( url )
+    end
 private
     def current_user
         @user ||= User.where( id: session[:user_id] ).first if session[:user_id]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,14 @@ class SessionsController < ApplicationController
     def create
         omniauth = request.env['omniauth.auth']
         info = omniauth['info']['email']
+        continue_url = request.env['omniauth.params']['continue']
+        redirect_path = (url_absolute? continue_url) ? '/' : continue_url
         @auth = Authorization.where( provider: omniauth['provider'], uid: omniauth['uid'] ).first
         if @auth
             user = User.where( id: @auth.user_id ).first
             if user
                 session[:user_id] = @auth.user_id
-                redirect_to '/'
+                redirect_to redirect_path
                 flash.notice = "Signed in!"
             else
                 puts "[FATAL] Failure: While an authorization was found for this provider (#{omniauth['provider']}) and uid, we could not find the referenced Bikboo user account (user_id: #{@auth.user_id}). This indicates an underlying failure with the database, destroying authorization."
@@ -26,7 +28,7 @@ class SessionsController < ApplicationController
                     new_auth = Authorization.create( uid: omniauth['uid'], provider: omniauth['provider'], user_id: user.id )
                     if new_auth and not new_auth.new_record?
                         session[:user_id] = new_auth.user_id
-                        redirect_to '/'
+                        redirect_to redirect_path
                         flash.notice = "Signed up and logged in. Welcome!"
                     else
                         flash.alert = "Failed to signup, server error. Please try again later"
