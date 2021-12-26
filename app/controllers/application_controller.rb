dbdbb003a6b2948dee27351f39e0224df7f2fd89
Rodion <rodion.developer1@gmail.com>
2021-12-26 08:24:27 +0000

    Created 'require_login' private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
     SITE_NAME = "Bikboo"
     protect_from_forgery with: :exception
-    def url_absolute?(url)
-        /^https?:\/\//i.match? ( url )
-    end
 private
+    def require_login(to_access: false)
+        unless current_user
+            flash.alert = "You must be logged in to access this page. Please sign in with Google."
+            redirect_to to_access ? "/?continue=#{to_access}" : "/"
+        end
+    end
+    def url_absolute?(url)
+        if block_given?
+            yield url, ( /^https?:\/\//i.match? url ) ? url : false
+        else
+            /^https?:\/\//i.match? ( url )
+        end
+    end
     def current_user
         @user ||= User.where( id: session[:user_id] ).first if session[:user_id]
     end
-    helper_method :current_user
+    helper_method :current_user, :url_absolute?
 end
