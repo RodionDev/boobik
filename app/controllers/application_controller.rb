619fdb704d47a09de077f478398e504f325a46ab
Rodion <rodion.developer1@gmail.com>
2022-02-24 05:34:27 +0000

    Removed root_url (/) fallback for require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
     before_action :validate_session
 private
     def require_login(to_access: false)
-        to_access ||= request.fullpath
         unless current_user
-            redirect_to( to_access ? "/?continue=#{to_access}" : "/", alert: "You must be logged in to access this page. Please sign in with Google." )
+            redirect_to( "/?continue=#{to_access or request.fullpath}", alert: "You must be logged in to access this page. Please sign in with Google." )
         end
     end
     def url_absolute?(url)
