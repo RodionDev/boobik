de76334ba238450c36179fe3c10ff357dbfde39b
Rodion <rodion.developer1@gmail.com>
2023-09-03 16:52:06 +0000

    Renamed project to 'Bikboo' (from 'HerokuApp')

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 require 'rails/all'
 Bundler.require(*Rails.groups)
-module HerokuApp
+module Bikboo
   class Application < Rails::Application
     config.load_defaults 5.1
     config.sass.preferred_syntax = :sass
